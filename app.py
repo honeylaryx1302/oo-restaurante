c168c205d781dc91d835512b1d9dd47bdf0307a1
honeylaryx1302 <162998971+honeylaryx1302@users.noreply.github.com>
2024-05-27 12:10:32 -0300

    Add files via upload

--- app.py
+++ app.py
@@ -1,11 +1,18 @@
-from restaurante import Restaurante
+from Classes.restaurante import Restaurante
 
-restaurante_praca = Restaurante('praca', 'Gourmet')
-restaurante_mexicano = Restaurante('Mexican food', 'Mexicana')
-restaurante_japones = Restaurante('japa', 'japonesa')
+restaurante_praca = Restaurante('praça', 'Gourmet')
+restaurante_praca.receber_avaliacao('Gui', 10)
+restaurante_praca.receber_avaliacao('Lais', 8)
+restaurante_praca.receber_avaliacao('Emy', 2)
 
-restaurante_mexicano.alternar_estado()
+restaurante_mexicano = Restaurante ('mexicano','comida')
+restaurante_mexicano.receber_avaliacao('gui',5)
 
+restaurante_Larissa = Restaurante ('larissinha', 'X-salada')
+restaurante_Larissa.receber_avaliacao('Daniel', 4.2)
+restaurante_Larissa.receber_avaliacao('Gaby',3.2)
+
+restaurante_Larissa.alternar_estado()
 
 def main():
     Restaurante.listar_restaurantes()
